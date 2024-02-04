666c2892d7ffa2e41b15802a36de4b06e9e4f591
Krishna <nayak100krishna@gmail.com>
2024-02-04 20:23:50 +0530

    Multi Page template degisn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import MultiSelectOption from "../components/ui/Dropdown/MultiSelectOption";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div>
+        <MultiSelectOption />
+      </div>
+    </div>
+  );
 }
