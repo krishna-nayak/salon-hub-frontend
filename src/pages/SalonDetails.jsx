73f034e3610a2a1f1b4fcf9004f066710fca3a45
Manisha Swain <manishaswain2003@gmail.com>
2024-02-14 01:37:47 +0530

    Minor changes

--- src/pages/SalonDetails.jsx
+++ src/pages/SalonDetails.jsx
@@ -151,13 +151,13 @@ export default function Modal() {
                   </div>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                <div className="flex gap-2 items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="btn w-20"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Close
+                    CLOSE
                   </button>
                   <button
                     className="btn w-20"
